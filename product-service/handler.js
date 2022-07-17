690bc123f39f74a10b8e6f43986f6c113cdf7489
maria-ta <marytana58@gmail.com>
2022-07-17 23:53:26 +0200

    Add another endpoint

--- product-service/handler.js
+++ product-service/handler.js
@@ -19,3 +19,13 @@ module.exports.getProductsList = async (event) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.getProduct = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(ALL_PRODUCTS[0]),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
